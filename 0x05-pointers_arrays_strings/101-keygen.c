cb594d59cf930884b22f3361ff8de3e7e2ebce4a
Silywayne11 <silywayne@gmail.com>
2023-02-27 10:04:03 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,3 +1,5 @@
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -29,8 +31,10 @@ int main(void)
 	{
 	diff_half1 = (sum - 2772) / 2;
 	diff_half2 = (sum - 2772) / 2;
+
 	if ((sum - 2772) % 2 != 0)
 	diff_half1++;
+
 	for (index = 0; password[index]; index++)
 	{
 	if (password[index] >= (33 + diff_half1))
