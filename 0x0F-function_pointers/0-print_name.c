d4fc957c229e416e6fe7c7f1646d4dfb0b72a916
Silywayne11 <silywayne@gmail.com>
2023-03-22 01:11:38 -0700

    0.What is my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - A function that prints a name.
+ * @name: name of a person.
+ * @f: print name as is and UPPERCASE.
+ * Return: Always 0.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if  (name == NULL || f == NULL)
+		return;
+	f(name);
+}
