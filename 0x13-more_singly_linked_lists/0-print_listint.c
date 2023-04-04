ccc501f27005e58f9de37966b5e6834a2d091e6e
Silywayne11 <silywayne@gmail.com>
2023-04-04 02:34:59 -0700

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,16 +10,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-size_t n_nodes = 0;
-
-if (!h)
-return (0);
+size_t nodes = 0;
 
 while (h)
 {
 printf("%d\n", h->n);
 h = h->next;
-n_nodes++;
+nodes++;
 }
-return (n_nodes);
+
+return (nodes);
 }
